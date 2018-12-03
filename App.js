9771f3d67b51d52df8ab6311aac8615758d4cd8b
Jan Miszura <jan@ilion.com.br>
2018-12-03 13:27:56 -0200

    implementado SplashScreen Android

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View, StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -10,6 +11,11 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+  
   render() {
     return (
       <View style={styles.container}>
